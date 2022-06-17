912c8b5e6f08bc1f02de9dafe784765444727edc
PavelOlofinskiy <pavelolofinskiy@gmail.com>
2022-06-17 12:20:24 +0300

    create database and add it to the project

--- ToDoListSQL/settings.py
+++ ToDoListSQL/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'ToDoListSQL.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'TodoDB',
+        'USER':'postgres',
+        'PASSWORD':'postgres',
+        'HOST':'localhost',
+
     }
 }
 
--- base/models.py
+++ base/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 
+class Todo(models.Model):
+    content = models.TextField()
